272f1be6dd2a636ff8fa2320c86fc84967b6662d
Jeroen Gunster <jeroen.gunster@gmx.com>
2023-11-02 18:22:34 +0100

    added social icons

--- components/socials/Socials.js
+++ components/socials/Socials.js
@@ -1,3 +1,12 @@
+import { Facebook, Instagram } from "iconoir-react";
+import styles from "./Socials.module.css";
+
 export default function Socials() {
-  return <div>Socials</div>;
+  return (
+    <div className={styles["icon-container"]}>
+      <Facebook />
+      <img src="/discogs.svg" alt="discogs-logo" />
+      <Instagram />
+    </div>
+  );
 }
