ab7595f90efc982e17d7f269d2f28a0f87a8123d
Jeroen Gunster <jeroen.gunster@gmx.com>
2023-11-02 18:26:27 +0100

    add urls to socials

--- components/socials/Socials.js
+++ components/socials/Socials.js
@@ -1,12 +1,19 @@
 import { Facebook, Instagram } from "iconoir-react";
+import Link from "next/link";
 import styles from "./Socials.module.css";
 
 export default function Socials() {
   return (
     <div className={styles["icon-container"]}>
-      <Facebook />
-      <img src="/discogs.svg" alt="discogs-logo" />
-      <Instagram />
+      <Link href="https://www.facebook.com/guyvannueten/">
+        <Facebook />
+      </Link>
+      <Link href="https://www.discogs.com/artist/385715-Guy-Van-Nueten?filter_anv=0&type=Credits">
+        <img src="/discogs.svg" alt="discogs-logo" />
+      </Link>
+      <Link href="https://www.instagram.com/guy.van.nueten/">
+        <Instagram />
+      </Link>
     </div>
   );
 }
