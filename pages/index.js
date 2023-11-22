4bb8e98be95376653167ffeb571e38d061ef13f2
Jeroen Gunster <jeroen.gunster@gmx.com>
2023-11-22 15:49:15 +0100

    add seo metadata

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,66 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>
+          Guy van Nueten – Belgian Composer & Pianist | Official Music Site
+        </title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Explore the sonic world of Guy van Nueten, a renowned Belgian composer and pianist. Dive into his genre-defying music, from Belpop classics like April and June to contemporary classical masterpieces. Discover his latest albums and compositions on the official site."
+        />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Guy van Nueten – Belgian Composer & Pianist | Official Music Site"
+        />
+        <meta
+          property="og:description"
+          content="Belgian musician Guy van Nueten's official website features a unique interactive music player, genre selection, and exclusive content on his celebrated compositions and recordings."
+        />
+        <meta
+          property="og:image"
+          content="https://www.guyvannueten.com/guyvannueten.webp"
+        />
+        <meta property="og:url" content="https://www.guyvannueten.com" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Guy van Nueten – Belgian Composer & Pianist | Official Music Site"
+        />
+        <meta
+          name="twitter:description"
+          content="Belgian musician Guy van Nueten's official website features a unique interactive music player, genre selection, and exclusive content on his celebrated compositions and recordings."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.guyvannueten.com/guyvannueten.webp"
+        />
+
+        {/* Structured Data */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "http://schema.org",
+            "@type": "MusicGroup",
+            name: "Guy van Nueten",
+            url: "https://www.guyvannueten.com",
+            image: "https://www.guyvannueten.com/guyvannueten.webp",
+            description:
+              "Belgian composer and pianist known for his genre-defying music and collaborations across Europe.",
+            album: ["Merg", "Pacman", "Contact"],
+            member: [
+              {
+                "@type": "Person",
+                name: "Guy van Nueten",
+                sameAs: "http://www.wikipedia.org/Guy_van_Nueten",
+              },
+            ],
+          })}
+        </script>
       </Head>
       <GenreContext.Provider
         value={{
