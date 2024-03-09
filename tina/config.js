9133ef26749a4469d163a32a16f32a92150c1cc5
Jeroen Gunster <jeroen.gunster@gmx.com>
2024-03-09 14:52:11 +0100

    configured search

--- tina/config.js
+++ tina/config.js
@@ -29,6 +29,15 @@ export default defineConfig({
     },
   },
 
+  search: {
+    tina: {
+      indexerToken: process.env.TINA_SEARCH_TOKEN,
+      stopwordLanguages: ["eng"],
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100,
+  },
+
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
