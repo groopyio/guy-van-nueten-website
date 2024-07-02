ee860de65342999975ca454b1642935b870dfefd
Jeroen Gunster <jeroen.gunster@gmx.com>
2024-07-02 14:58:57 +0200

    usage media style

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,23 +3,26 @@ import MediaPlayer from "@components/media-player/MediaPlayer";
 import { SourceLinks } from "@components/media-player/source-links/SourceLinks";
 import Socials from "@components/socials/Socials";
 import SongkickToggle from "@components/songkick/toggle/SongkickToggle";
-import { useDeviceQuery } from "@hooks/useDeviceQuery";
+import { Media, MediaContextProvider } from "@hooks/useMediaStyle";
 import { AudioMetaContext } from "pages";
 import { useContext } from "react";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
   const { song } = useContext(AudioMetaContext);
-  const { isMobile } = useDeviceQuery();
   return (
-    <footer className={styles["footer"]}>
-      <div className={styles["footer-icons"]}>
-        <Socials />
-        {isMobile && <SourceLinks song={song} />}
-        <SongkickToggle />
-        <Contact />
-      </div>
-      <MediaPlayer />
-    </footer>
+    <MediaContextProvider>
+      <footer className={styles["footer"]}>
+        <div className={styles["footer-icons"]}>
+          <Socials />
+          <Media at="mobile">
+            <SourceLinks song={song} />
+          </Media>
+          <SongkickToggle />
+          <Contact />
+        </div>
+        <MediaPlayer />
+      </footer>
+    </MediaContextProvider>
   );
 }
